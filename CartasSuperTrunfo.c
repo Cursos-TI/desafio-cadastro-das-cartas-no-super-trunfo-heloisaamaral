ec942d2d369a88cd657a844d32f9b91816d04faf
heloisaamaral <heloisa.amaral250@gmail.com>
2025-08-25 20:48:25 +0000

    comparei as cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,9 +4,9 @@ int main() {
     char estado[50];
     char código [80];
     char cidade [60];
-    int população;
-    float área;
-    float PIB;
+    int população1, população2;
+    float área1, área2;
+    float PIB1, PIB2;
     int pontosturisticos;
     float densidadepopulacional;
     float PIBpercapita;
@@ -24,22 +24,22 @@ int main() {
     scanf("%s", &cidade);
 
     printf("Digite a população: \n");
-    scanf("%d", &população);
+    scanf("%d", &população1);
 
     printf("Digite a área em km²: \n");
-    scanf("%f", &área);
+    scanf("%f", &área1);
 
     printf("Digite o valor do PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB1);
 
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontosturisticos);
 
-    densidadepopulacional = população / área;
+    densidadepopulacional = população1 / área1;
 
     printf("A densidade populacional é: %.2f\n", densidadepopulacional);
 
-    PIBpercapita = PIB / população;
+    PIBpercapita = PIB1 / população1;
 
     printf("O PIB per capita é: %.2f\n", PIBpercapita);
     
@@ -56,25 +56,41 @@ int main() {
     scanf("%s", &cidade);
 
     printf("Digite a população: \n");
-    scanf("%d", &população);
+    scanf("%d", &população2);
 
     printf("Digite a área em km²: \n");
-    scanf("%f", &área);
+    scanf("%f", &área2);
 
     printf("Digite o valor do PIB: \n");
-    scanf("%f", &PIB);
+    scanf("%f", &PIB2);
 
     printf("Digite a quantidade de pontos turísticos: \n");
     scanf("%d", &pontosturisticos);
 
-    densidadepopulacional = população / área;
+    densidadepopulacional = população2 / área2;
 
     printf("A densidade populacional é: %.2f\n", densidadepopulacional);
 
-    PIBpercapita = PIB / população;
+    PIBpercapita = PIB2 / população2;
 
     printf("O PIB per capita é: %.2f\n", PIBpercapita);
 
+
+    if (população1 > população2){
+        printf("Carta1 venceu!\n");
+        
+    } else {
+        printf("Carta2 venceu!\n");
+    }
+
+    if (área1 > área2){
+        printf("Carta1 venceu!\n");
+        
+    } else {
+        printf("Carta2 venceu!\n");
+    }
+    
+    
     return 0;
 
 }
